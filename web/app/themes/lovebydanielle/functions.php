69241835776f37e47af692a56ba0ffb503d73cdf
Paul Valenzano <paul.valenzano@gmail.com>
2019-08-18 17:16:40 -0700

    global styles, logo placement tweak

--- web/app/themes/lovebydanielle/functions.php
+++ web/app/themes/lovebydanielle/functions.php
@@ -2,6 +2,7 @@
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-main-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-main-style', get_stylesheet_directory_uri() . '/style.css', array('parent-main-style') );
+    wp_enqueue_style( 'child-global-style', get_stylesheet_directory_uri() . '/styles/globals.css', array('parent-main-style') );
     wp_enqueue_style( 'child-forms-style', get_stylesheet_directory_uri() . '/styles/forms.css', array('parent-main-style') );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
